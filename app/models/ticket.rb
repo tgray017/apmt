98b65bc95f41d48e21cf4495f2cf3422caf8d932
Thomas Gray <thomas.gray@tgray.local>
2019-09-29 11:50:14 -0400

    Build associations for comments

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,5 @@ class Ticket < ApplicationRecord
   belongs_to :assignee, :class_name => "User"
   alias_attribute :user_id, :assignee_id
 
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tickets
+  has_many :comments, through: :tickets
 
   def assigned_tickets
     Ticket.where(:assignee_id => self.id)

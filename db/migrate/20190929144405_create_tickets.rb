1efd7f97c2b35bb4bc581049018accf5723b4910
Thomas Gray <thomas.gray@tgray.local>
2019-09-29 10:44:47 -0400

    Create Ticket models, views, routes and migrations

---
+++ db/migrate/20190929144405_create_tickets.rb
@@ -0,0 +1,12 @@
+class CreateTickets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.string :description
+      t.integer :creator_id
+      t.integer :assignee_id
+
+      t.timestamps
+    end
+  end
+end

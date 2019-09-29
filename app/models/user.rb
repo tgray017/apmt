1c81656c65476b1f5c1ce36e323620f4b224a4c8
Thomas Gray <thomas.gray@tgray.local>
2019-09-29 11:40:48 -0400

    Create assigned_tickets and created_tickets User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_many :tickets
+
+  def assigned_tickets
+    Ticket.where(:assignee_id => self.id)
+  end
+
+  def created_tickets
+    Ticket.where(:creator_id => self.id)
+  end
+
 end

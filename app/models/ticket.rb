e021b6d70b5d2d64da8bd04905a4e627e10c85fa
Thomas Gray <thomas.gray@tgray.local>
2019-09-29 11:37:28 -0400

    Associate creators and assignees with tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,8 @@
 class Ticket < ApplicationRecord
+  belongs_to :creator, :class_name => "User"
+  alias_attribute :user_id, :creator_id
+
+  belongs_to :assignee, :class_name => "User"
+  alias_attribute :user_id, :assignee_id
+
 end

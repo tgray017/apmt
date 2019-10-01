41c7129ab3b3c43dccc8569754228302e3f25048
Thomas Gray <thomas.gray@tgray.local>
2019-09-30 21:49:37 -0400

    Create new and create actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # why isn't it failing if password_digest != password_confirmation
+    if @user.save
+      redirect_to tickets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :is_admin)
+    end
 end

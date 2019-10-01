8414ae38ac5ec1c6f9e1db97b988c3c2c1010675
Thomas Gray <thomas.gray@tgray.local>
2019-09-30 20:55:27 -0400

    Create welcome and sessions controller and home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses
   resources :tickets
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+  root 'welcome#home'
 end

92cc6849c10e42ae577b80e20cc118ed667ae816
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-10-02 22:23:58 -0400

    Refactor ticket table into partial, create show action for statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,7 @@
 class StatusesController < ApplicationController
+
+  def show
+    @status = Status.find(params[:id])
+    @tickets = Ticket.where(:status_id => @status.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :statuses
   resources :tickets
-  resources :users
+  resources :users do
+    resources :tickets
+  end
   get 'signin', to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   root 'welcome#home'
+
 end

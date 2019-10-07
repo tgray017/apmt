cfecf1418b4ae273876666305423d4a001e82120
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-10-06 21:20:36 -0400

    Add replies to ticket show view, adjust CSS for readability

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,18 @@
 class RepliesController < ApplicationController
 
   def create
-    raise params.inspect
+    @comment = Comment.find(params[:reply][:comment_id])
+    @reply = @comment.replies.build(:content => params[:reply][:content], :user => current_user, :comment => @comment)
+    if @reply.save
+      redirect_to ticket_path(@comment.ticket)
+    else
+      render ticket_path(@comment.ticket)
+    end
   end
+
+  #private
+
+    #def reply_params
+    #  params.require(:reply).permit(:comment_id, :content)
+    #end
 end

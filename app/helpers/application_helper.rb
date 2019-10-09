e8f70f93ce583672c8b127ca89e01c950af4ed8b
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-10-08 22:05:29 -0400

    Fix navbar to highlight on current_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def navbar_highlight(path)
+    if path == current_path
+      "w3-bar-item w3-button w3-padding-large w3-white"
+    else
+      "w3-bar-item w3-button w3-padding-large w3-hover-white"
+    end
+  end
+
+  def current_path
+    request.env["REQUEST_PATH"]
+  end
 end

0238cad84b9a7a9d67d700ac10d96f8c811283f2
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-11-06 08:01:34 -0500

    Adding #due_this_week and #past_due scope methods to tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,8 @@
 class Ticket < ApplicationRecord
 
+  scope :due_this_week, -> { where('due_date <= ?', Time.now + 1.week) }
+  scope :past_due, -> { where('due_date <= ?', Time.now) }
+
   belongs_to :assignee, :class_name => "User", :foreign_key => "assignee_id"
   belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
 
